2d07f5745dc129f1041ca952a49301ae8baf3366
KilianPauwels <Kilianpauwels@gmail.com>
2016-12-08 14:05:52 +0100

    WebApi werkt mogelijk

---
+++ WebApi/WebApi/Controllers/DefaultController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        [Route("getData")]
+        [HttpGet]
+        public IHttpActionResult GetData()
+        {
+            return Ok();
+        }
+
+        
+        [HttpPost]
+        public IHttpActionResult PostData(DroneData dronedata)
+        {
+            
+            return Ok();
+        }
+
+        [HttpOptions]
+        public IHttpActionResult Options()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ WebApi/WebApi/Models/DroneData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class DroneData
+    {
+        public double Temperature { get; set; }
+        public double WindSpeed { get; set; }
+        public double WindOrientation { get; set; }
+        public double ParticleSize { get; set; }
+        public string TimeStamp { get; set; }
+
+    }
+}
